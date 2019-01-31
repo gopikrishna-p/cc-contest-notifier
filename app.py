34aeb73b8d86d3ace9412c537b8d89aadd7fff1d
Gopikrishna Pavuluri <gopikrishnapavuluri23@gmail.com>
2019-01-31 21:41:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/test")
-def hello():
-    return render_template('index.html')
+@app.route('/test')
+def hello_admin():
+   return 'Hello'
+   
+@app.route('/testt')
+def hello_user(name):
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+   app.run(debug = True)
