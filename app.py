4503796fa23e59a20cd36e397df25fe5aaf36703
Gopikrishna Pavuluri <gopikrishnapavuluri23@gmail.com>
2019-01-31 21:02:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-from flask import Flask,render_template,jsonify
-from flask import request
-import json
-import os
-import mysql.connector
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/contest')
-def holidify():
-	#return "The placeCode is " + placeCode
-
-
-	#testjsoon()
-	#return json.dumps(json_data)
-	return render_template('/index.html')
-
-
-
-
-if __name__ == '__main__':
-	app.run(debug=True)
+@app.route("/")
+def hello():
+    return "Hello World!"
